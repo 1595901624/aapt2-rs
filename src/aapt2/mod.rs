946b5a419fd14d54225864cd3224a90f49742e77
luhaoyu <haoyu3@163.com>
2024-07-18 17:18:16 +0800

    pub

--- src/aapt2/mod.rs
+++ src/aapt2/mod.rs
@@ -1,27 +1,27 @@
 use std::path::PathBuf;
 
 #[cfg(target_os = "windows")]
-mod win;
+pub mod win;
 
 #[cfg(target_os = "linux")]
 mod linux;
-mod aapt2;
+pub mod aapt2;
 
 #[cfg(target_os = "macos")]
-mod macos;
+pub mod macos;
 
 #[cfg(target_os = "windows")]
-fn get_exe_path() -> PathBuf {
+pub fn get_exe_path() -> PathBuf {
     return win::get_exe_path();
 }
 
 #[cfg(target_os = "linux")]
-fn get_exe_path() -> PathBuf {
+pub fn get_exe_path() -> PathBuf {
     return linux::get_exe_path();
 }
 
 #[cfg(target_os = "macos")]
-fn get_exe_path() -> PathBuf {
+pub fn get_exe_path() -> PathBuf {
     return macos::get_exe_path();
 }
 
