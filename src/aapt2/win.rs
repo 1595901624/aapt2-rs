30a7986fbf9361f894943b391ff1c4342ac726ab
luhaoyu <haoyu3@163.com>
2024-07-18 17:12:16 +0800

    x

--- src/aapt2/win.rs
+++ src/aapt2/win.rs
@@ -2,11 +2,11 @@ use std::env;
 use std::path::PathBuf;
 
 /// exe 路径
-/// windows: OUT_DIR/assets/win/aapt2.exe
+/// windows: OUT_DIR/assets/win/aapt2-x86_64-pc-windows-msvc.exe
 pub(crate) fn get_exe_path() -> PathBuf {
     let out_dir = env::var("OUT_DIR").unwrap();
     // linux: OUT_DIR/assets/linux/aapt2
-    return PathBuf::from(out_dir).join("assets").join("win").join("aapt2.exe");
+    return PathBuf::from(out_dir).join("assets").join("win").join("aapt2-x86_64-pc-windows-msvc.exe");
 }
 
 
--- src/lib.rs
+++ src/lib.rs
@@ -15,9 +15,9 @@ mod tests {
         // let result = add(2, 2);
         // assert_eq!(result, 4);
         let out_dir = env::var("OUT_DIR").unwrap();
-        // windows: OUT_DIR/assets/win/aapt2.exe
+        // windows: OUT_DIR/assets/win/aapt2-x86_64-pc-windows-msvc.exe
         // linux: OUT_DIR/assets/linux/aapt2
-        let aapt2_path = PathBuf::from(out_dir).join("assets").join("win").join("aapt2.exe");
+        let aapt2_path = PathBuf::from(out_dir).join("assets").join("win").join("aapt2-x86_64-pc-windows-msvc.exe");
         println!("aapt2_path => {:?}", aapt2_path);
     }
 }
